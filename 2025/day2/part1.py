3364fce22f1a3b3eec5906469e8bb2d208d3826c
mkalinowsk1 <mical7360@gmail.com>
2025-12-02 14:01:08 +0100

    day2

---
+++ 2025/day2/part1.py
@@ -0,0 +1,43 @@
+with open ("input.in") as fin:
+	line = fin.read().split(",")
+
+sum = 0
+ranges = []
+largest_num = 0
+for r in line:
+	start, end = r.split("-")
+	ranges.append((int(start), int(end)))
+
+	if int(end) > largest_num:
+		largest_num = int(end)
+
+print(ranges)
+
+def generate_invalid_ids(largest):
+	x = 1
+	invalids = []
+
+	while True:
+		x_str = str(x)
+
+		i_str = x_str * 2
+		I = int(i_str)
+
+		if I > largest:
+			break
+		invalids.append(I)
+		x += 1
+	return invalids
+
+possible_invalid_ids = generate_invalid_ids(largest_num)
+
+
+for r in ranges:
+	for id in possible_invalid_ids:
+		if id >= r[0] and id <= r[1]:
+			sum += id
+
+
+
+print(sum)
+
